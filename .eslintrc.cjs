f37dd67ea500010b1083770f3d78d1d640e12f6e
d982h8st7 <sinnarver15@gmail.com>
2023-10-30 00:05:49 +0900

    Add ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  }
+};
